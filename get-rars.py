064164a60bba36aed0f5dfaf76f31e487f7af46d
Obijuan <obijuan.cube@gmail.com>
2024-06-22 13:44:08 +0200

    wiki: add image

--- get-rars.py
+++ get-rars.py
@@ -1,2 +1,8 @@
+from colorama import Fore, Style
+
 print("Hola...")
 print("Vaamos!")
+print(Fore.RED + "Texto en Rojo!!")
+print(Fore.GREEN + "Texto en verde")
+print(Style.RESET_ALL)
+print("Normal")
