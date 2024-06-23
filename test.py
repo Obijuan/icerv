791a6e55cf209790ed18be2a1a441d9b39a2814a
Obijuan <obijuan.cube@gmail.com>
2024-06-23 17:51:33 +0200

    initial test

---
+++ test.py
@@ -0,0 +1,20 @@
+"""Simple test for the get_rars script"""
+
+import unittest
+
+
+class Test1(unittest.TestCase):
+    """Initial tests"""
+
+    # --- All the test methods defined should start with
+    # --- the name "test"
+
+    def test_msg1(self):
+        """Testing..."""
+
+        a = True
+        self.assertTrue(a)
+
+
+if __name__ == "__main__":
+    unittest.main()
