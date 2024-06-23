4747442cec7024457dcd416e3b08789b597f95df
Obijuan <obijuan.cube@gmail.com>
2024-06-23 19:50:58 +0200

    tests

--- get_rars.py
+++ get_rars.py
@@ -5,11 +5,14 @@ from colorama import Fore, Style
 
 def main():
     print("Hola...")
+    
+
+if __name__ == '__main__':
+    main()
+    
     print("Vaamos!")
     print(Fore.RED + "Texto en Rojo!!")
     print(Fore.GREEN + "Texto en verde")
     print(Style.RESET_ALL)
     print("Normal")
-
-if __name__ == '__main__':
-    main()
+    
--- test_get_rars.py
+++ test_get_rars.py
@@ -1,17 +1,31 @@
+"""Testing the get_rars modules"""
+
 import unittest
+from unittest.mock import patch
+from io import StringIO
+
 
 # -- Module to test
 import get_rars as gr
 
+
 class TestInitial(unittest.TestCase):
+    """Initial tests"""
 
-    def test_output(self):
-        print("Testing...")
-        a = True
-        self.assertTrue(a)
+    # -- Redirect the standard output
+    @patch("sys.stdout", new_callable=StringIO)
+    def test_main_output(self, stdout):
+        """Testing the output of the main function"""
+
+        # -- Function to test
+        gr.main()
+
+        # -- Read its stdoutput
+        output = stdout.getvalue()
+
+        # -- Check the output
+        self.assertEqual(output, "Hola...\n")
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
-
